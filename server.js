344a5602b601421e8c83170a756a9d8b36d66d20
Harish <htata31@gmail.com>
2019-04-07 17:29:14 -0500

    server changes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var url = 'mongodb://htata31:tata1994@ds135993.mlab.com:35993/htata';
 
 
 app.get('/', function(req, res) {
-    res.render('/html/homepage.html');
+    res.render('index.html');
 });
 
 //In the below "get method" the input is lat and long.
